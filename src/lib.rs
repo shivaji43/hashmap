de2fb9e8583daf95e85898b1e69636049124980c
shivaji43 <shivajijraut@gmail.com>
2025-08-01 19:57:25 +0530

    create and insert

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::mem;
+const INITIAL_NBUCKETS: usize = 1;
+
+pub struct HashMap<K, V> {
+    buckets: Vec<Vec<(K, V)>>,
+    items: usize,
+}
+
+impl<K, V> HashMap<K, V> {
+    pub fn new() -> Self {
+        HashMap {
+            buckets: Vec::new(),
+            items: 0,
+        }
+    }
+}
+
+impl<K, V> HashMap<K, V>
+where
+    K: Hash + Eq,
+{   
+
+    // takes key and returns bucket's index to insert the key value pair in that specific bucket
+    fn bucket(&self , key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        (hasher.finish() % (self.buckets.len() as u64)) as usize        
+    }
+
+
+    // finds the hash and puts <key , value> pair in the bucket vector
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
+        }
+
+        let bucket: usize = self.bucket(&key);
+
+        let bucket = &mut self.buckets[bucket];
+
+        for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
+            if ekey == &key {
+                return Some(mem::replace(evalue, value));
+            }
+        }
+
+        bucket.push((key, value));
+        None
+    }
+
+
+    // function to resize the buckets inside a hashmap if the capacity exceeds 3/4th of the size of a bucket to optimize search time
+    fn resize(&mut self) {
+        let target_size: usize = match self.buckets.len() {
+            0 => INITIAL_NBUCKETS,
+            n => 2 * n,
+        };
+
+        let mut new_buckets = Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new()));
+
+        for (key, value) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
+            let mut hasher = DefaultHasher::new();
+            key.hash(&mut hasher);
+            let bucket = (hasher.finish() % new_buckets.len() as u64) as usize;
+            new_buckets[bucket].push((key, value));
+        }
+        mem::replace(&mut self.buckets, new_buckets);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn insert() {
+        let mut map = HashMap::new();
+        map.insert("shivaji", 1);
+    }
+}
